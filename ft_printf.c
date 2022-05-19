2af061ff87161fed4b5a3afe79db5c7c75b8e9c4
wportilh <wportilh@student.42sp.org.br>
2022-05-20 01:33:20 +0200

    works

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 22:13:04 by wportilh          #+#    #+#             */
-/*   Updated: 2022/05/19 18:10:56 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/05/20 01:29:22 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,6 +107,117 @@ void	ft_put_uns_nbr(unsigned int n, int *index, int *len)
 	}
 }
 
+void	ft_uitohex_p(size_t n, int *index, int *len)
+{
+	size_t			cp;
+	int				size_memb;
+	char			*str;
+
+	cp = n;
+	size_memb = 1;
+	if (n == 0)
+	{
+		write (1, "(nil)", 5);
+		*len = *len + 5;
+		*index = *index + 2;
+		return ;
+	}
+	if (cp > 15)
+	{
+		while (cp > 15)
+		{
+			cp = cp / 16;
+			size_memb++;
+		}
+	}
+	str = malloc((size_memb * sizeof(char)) + 3);
+	str[size_memb] = '\0';
+	while (n > 15)
+	{
+		if ((n % 16) > 9)
+			str[--size_memb] = (n % 16) + 87;
+		else
+			str[--size_memb] = (n % 16) + 48;
+		n = n /16;
+	}
+	if (n > 9)
+		str[--size_memb] = n + 87;
+	else
+		str[--size_memb] = n + 48;
+	write(1, "0x", 2);
+	*len = *len + 2;
+	ft_putstr(str, index, len);
+	free (str);
+}
+
+void	ft_uitohex(unsigned int n, int *index, int *len)
+{
+	unsigned int	cp;
+	int				size_memb;
+	char			*str;
+
+	cp = n;
+	size_memb = 1;
+	if (cp > 15)
+	{
+		while (cp > 15)
+		{
+			cp = cp / 16;
+			size_memb++;
+		}
+	}
+	str = malloc((size_memb * sizeof(char)) + 1);
+	str[size_memb] = '\0';
+	while (n > 15)
+	{
+		if ((n % 16) > 9)
+			str[--size_memb] = (n % 16) + 87;
+		else
+			str[--size_memb] = (n % 16) + 48;
+		n = n /16;
+	}
+	if (n > 9)
+			str[--size_memb] = n + 87;
+		else
+			str[--size_memb] = n + 48;
+	ft_putstr(str, index, len);
+	free (str);
+}
+
+void	ft_uitohexup(unsigned int n, int *index, int *len)
+{
+	unsigned int	cp;
+	int				size_memb;
+	char			*str;
+
+	cp = n;
+	size_memb = 1;
+	if (cp > 15)
+	{
+		while (cp > 15)
+		{
+			cp = cp / 16;
+			size_memb++;
+		}
+	}
+	str = malloc(size_memb * sizeof(char) + 1);
+	str[size_memb] = '\0';
+	while (n > 15)
+	{
+		if ((n % 16) > 9)
+			str[--size_memb] = (n % 16) + 55;
+		else
+			str[--size_memb] = (n % 16) + 48;
+		n = n /16;
+	}
+	if (n > 9)
+			str[--size_memb] = n + 55;
+		else
+			str[--size_memb] = n + 48;
+	ft_putstr(str, index, len);
+	free (str);
+}
+
 int	ft_printf(const char *str, ...)
 {
 	va_list			ap;
@@ -133,6 +244,12 @@ int	ft_printf(const char *str, ...)
 			ft_putnbr(va_arg(ap, int), &index, &len);
 		if ((ptr[index] == '%') && (ptr[index + 1] == 'u'))
 			ft_put_uns_nbr(va_arg(ap, unsigned int), &index, &len);
+		if ((ptr[index] == '%') && (ptr[index + 1] == 'x'))
+			ft_uitohex(va_arg(ap, unsigned int), &index, &len);
+		if ((ptr[index] == '%') && (ptr[index + 1] == 'X'))
+			ft_uitohexup(va_arg(ap, unsigned int), &index, &len);
+		if ((ptr[index] == '%') && (ptr[index + 1] == 'p'))
+			ft_uitohex_p(va_arg(ap, size_t), &index, &len);
 	}
 	va_end(ap);
 	return (len);
